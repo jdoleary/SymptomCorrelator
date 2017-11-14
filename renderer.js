135560c1b7949e1fafb81e1c17063c073053443b
Jordan O'Leary <jdoleary@gmail.com>
2017-11-13 23:23:34 -0500

    feat (Input) Adds types: time, yesorno; adds better date and time format.  Adds default values when TAB

--- renderer.js
+++ renderer.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const _ = require('lodash');
 let data = {data:[]};
+let label, error, userInput;
+let waitForEnter = false;
 function save(output){
     const content = JSON.stringify(output);
 
@@ -20,23 +22,13 @@ function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
-let label, userInput;
-let waitForEnter = false;
 function ask(q, wait){
     label.innerHTML = q.q;
     waitForEnter = wait;
 }
-function answer(a){
-    console.log(a);
-    if(questions[qIndex].type == 'date'){
-        let date = new Date(a);
-        if(date.toString() != 'Invalid Date'){
-            a = date;
-        }else{
-            return invalid();
-        }
-    }
-    questions[qIndex].a = a;
+
+function fillAnswer(a){
+    questionCurrent.a = a;
     userInput.value = "";
     let isValid = true;
     if(isValid){
@@ -45,26 +37,48 @@ function answer(a){
             ask(nextQ,true);
         }
     }
-    
+  
+}
+function inputAnswer(a){
+    switch(questionCurrent.type){
+      case 'date':
+        let date = new Date(a);
+        if(date.toString() != 'Invalid Date'){
+            fillAnswer(a);
+            return;
+        }else{
+            invalid();
+            return;
+        }
+        break;
+      case 'time':
+      case 'yesorno':
+      default:
+        fillAnswer(a);
+    }    
 }
 function getNextQ(){
     if(++qIndex < questions.length){
-        return questions[qIndex];
+        questionCurrent = questions[qIndex];
+        return questionCurrent;
     }else{
         // restart and save
         qIndex = 0;
         data.data.push(_.map(questions, 'a'));
         updateHistory();
         save(data);
-        return questions[qIndex];
+        questionCurrent = questions[qIndex];
+        return questionCurrent;
     }
 }
 function invalid(){
-    label.innerHTML = 'Invalid, try again: ' + label.innerHTML;
+    error.innerHTML = 'Invalid, try again: ';
 }
 let qIndex = 0;
 let questions = [
-{q:'Event',a:''},
+{q:'Event',a:'',required:true},
+{q:'Negative of Event (y/n)',a:'',type:'yesorno'},
+{q:'Time',a:'',type:'time'},
 {q:'Date',a:'',type:'date'}
 ];
 function updateHistory(){
@@ -81,11 +95,14 @@ function updateHistory(){
 ready(()=>{
     
     var content=fs.readFileSync("./tmp/data.json", "utf8");
-    data =(JSON.parse(content));
+    if(content.length){
+      data =(JSON.parse(content));
+    }
     updateHistory();
     
     
     label = document.querySelector('#label');
+    error = document.querySelector('#error');
     //input
     userInput = document.querySelector('#user');
     userInput.focus();
@@ -94,19 +111,49 @@ ready(()=>{
         e = e || window.event;
         if(waitForEnter){
             if(e.code == 'Enter'){
-                answer(userInput.value);                
+                inputAnswer(userInput.value);                
             }
         }else{
             //answer immediately
-            answer(userInput.value);
+            inputAnswer(userInput.value);
         }
     };
     
-    
+    // Attempt to skip current input when TAB is pressed
+    // TODO: only tab, not mouseclick
     userInput.addEventListener("blur", function( event ) {
+      console.log(event);
+        // unless question is required
+        if(questionCurrent.required){
+          invalid();
+        }else{
+          // Skip current input with default, then refocus:
+          fillAnswer((function getDefaultAnswer(){
+            switch(questionCurrent.type){
+              case 'date':
+                return formatMMDDYYYY(new Date());
+                break;
+              case 'time':
+                return '00:00';
+              case 'yesorno':
+                return null;
+              default:
+                // Attempt to fill answer with content of input box.
+                return userInput.value;
+            }
+          })());
+        }
         userInput.focus();
     }, true);
     
-    ask(questions[qIndex], true);
     
+    function formatMMDDYYYY(date){
+      return (date.getMonth() + 1) + 
+      "/" +  date.getDate() +
+      "/" +  date.getFullYear();
+    }
+    
+    //Entry point!:
+    questionCurrent = questions[qIndex];
+    ask(questionCurrent, true);
 });
